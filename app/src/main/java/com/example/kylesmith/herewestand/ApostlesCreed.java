9477cde58b7ac5695749c42d7a32eca08037cd7b
kss003 <kss003@email.uark.edu>
2018-03-04 22:33:51 -0600

    Add files

---
+++ app/src/main/java/com/example/kylesmith/herewestand/ApostlesCreed.java
@@ -0,0 +1,10 @@
+package com.example.kylesmith.herewestand;
+
+import android.os.Bundle;
+
+public class ApostlesCreed extends  MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/example/kylesmith/herewestand/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.kylesmith.herewestand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemID = item.getItemId();
+
+        switch (itemID) {
+            case R.id.about:
+
+            case R.id.settings:
+
+        }
+        return true;
+    }
+
+
+}
